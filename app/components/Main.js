1c97987daa983f1ede4d3234c734f2d8833bf41f
iankit3 <a4ankit@outlook.com>
2016-11-30 18:45:55 +0530

    none

--- app/components/Main.js
+++ app/components/Main.js
@@ -11,7 +11,6 @@ class Main extends React.Component{
     }  
   }
 
-
   componentDidMount(){
      Data.getData('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1')
      .then( (res) => { this.setState({randomData:res});console.log(this);console.log(res) })
@@ -19,11 +18,9 @@ class Main extends React.Component{
   render(){
       return(
           <div className="col-md-9 full-height main">
-           
             <div className="row">
                 <SelectedUser selecteduser={this.props.selecteduser} />
             </div>
-
            <h1>Some bacon ipsum content</h1>
               <section>
                   <article>
